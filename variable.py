87a1db383652ab9218bafb0db7f9538404dd99fd
Rahul710537 <rahulbalu330@gmail.com>
2025-07-22 21:39:54 +0530

    Create variable.py

---
+++ variable.py
@@ -0,0 +1,15 @@
+# Variable Declaration and Initialization
+name = "Rahul"
+age = 20
+height = 5.9
+is_student = True
+
+# Displaying the variable values
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Is Student?", is_student)
+
+# Performing some operations
+next_year_age = age + 1
+print("Age next year will be:", next_year_age)
